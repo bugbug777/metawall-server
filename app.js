898fbc9f69ee5e13865733e52196d52f006ef022
sihle <gubug777@gmail.com>
2022-04-26 10:02:00 +0800

    connect MongoDB

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+mongoose.connect('mongodb://localhost:27017/meta').then((res) => {
+  console.log('資料庫連線成功！');
+});
 var app = express();
 
 app.use(cors());
