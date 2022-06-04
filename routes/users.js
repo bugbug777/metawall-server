93db3db0be55ff364feb583c20c911671619d871
sihle <gubug777@gmail.com>
2022-06-04 22:12:19 +0800

    修正 no return function error

--- routes/users.js
+++ routes/users.js
@@ -17,10 +17,10 @@ router.get('/', asyncErrorHandler(async (req, res, next) => {
 // 新增使用者，使用者註冊
 router.post('/', asyncErrorHandler(async (req, res, next) => {
   let { name, email, password } = req.body;
-  if (!name || !email || !password) appError(400, '欄位資訊不能為空！', next);
+  if (!name || !email || !password) return appError(400, '欄位資訊不能為空！', next);
 
   const hasEmail = await User.findOne({email: email}).exec();
-  if (hasEmail) appError(400, '該電子信箱已被使用者註冊！', next);
+  if (hasEmail) return appError(400, '該電子信箱已被使用者註冊！', next);
 
   const newUser = await User.create({
     name,
@@ -39,15 +39,15 @@ router.patch('/:id', asyncErrorHandler(async (req, res, next) => {
   const { name, gender, avatar } = req.body;
   const user = await User.findById(id).exec();
 
-  if (!user) appError(400, '找不到該名使用者資訊！', next);
+  if (!user) return appError(400, '找不到該名使用者資訊！', next);
 
-  if (!name || !gender) appError(400, '欄位資訊不能為空！', next);
+  if (!name || !gender) return  appError(400, '欄位資訊不能為空！', next);
 
-  const editedUser = await User.findByIdAndUpdate(id, {
-    name,
-    gender,
-    avatar
-  }, { new: true});
+  const editedUser = await User.findByIdAndUpdate(
+    id,
+    {name, gender, avatar},
+    {new: true}
+  );
   res.json({
     status: 'sucess',
     data: editedUser
